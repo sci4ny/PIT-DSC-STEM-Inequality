bf573d456e746b875e9c24a5d770fca92faac74a
ATEOTG <bryansantos9797@gmail.com>
2021-07-18 20:04:00 -0400

    added second argument for the fractions() function so that it better displays the ratio.

--- ShinyApp/dataLoad.R
+++ ShinyApp/dataLoad.R
@@ -5,7 +5,7 @@ library(dplyr)
 hs_data <- read.csv("~/PIT-DSC-STEM-Inequality/data/2018_HS_Data.csv")
 
 #turn decimal number in the ratio column into fractions, then turn fractions into ratios
-hs_data[,31] <- paste(fractions(hs_data[,31]), " ")
+hs_data[,31] <- paste(fractions(hs_data[,31], cycles = 3), " ")
 hs_data[,31] <- str_replace(hs_data[,31],"/", ":")
 
 
