f5f6d70aacc47578e20b8f8557484d40ca7b738b
ATEOTG <bryansantos9797@gmail.com>
2021-07-20 19:20:22 -0400

    added spaces to line to make code more readable, also added conditional panel if selecting High School Tab

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -12,20 +12,28 @@ shinyUI(
 fluidPage(
      #Title of Application
      titlePanel("NYC Schools"),
+
      #we have a sidebar with the grade range input and slider input
      sidebarLayout(
           sidebarPanel(
                h5("Data shown for each school are the most relevant metrics (found via MLR)
                   in predicting the outcome of STEM regents or math test results.")
                 ,
+
                #group box that allows user to select boroughs
                checkboxGroupInput("borough", label = "Borough to Include: ",
                                   choices = c("BRONX","BROOKLYN","MANHATTAN","QUEENS","STATEN IS"),
                                   selected = c("BRONX","BROOKLYN","MANHATTAN","QUEENS","STATEN IS")),
+
                #creates a slider that select what range of % poverty the user wants to see
                sliderInput("percent_poverty",
                            label = "Percent Poverty Range: ",
                            min = 0, max = 100, value = c(0,100)),
+
+               #because we want to display different sliders for specific grade ranges,
+               #we only include these sliders, when the tab selected is high school
+               conditionalPanel( condition = "input.CurrentTab == 'High School'",
+
                #creates slider input for STEM regent scores
                sliderInput("ccAlgebra",
                            label = "Mean Score for Common Core Algebra Range: ",
@@ -37,7 +45,8 @@ fluidPage(
                sliderInput("ccLE",
                            label = "Mean Score for Living Environment Range: ",
                            min = 0, max = 100, value = c(0,100)),
-          ),
+          )),
+
           #main panel will showcase the output (map)
           mainPanel(
                 #we create different panels for different grade ranges
