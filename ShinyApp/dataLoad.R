71bdc299ccee2389ae8186a7a9875efed6c897a4
nml2171 <86439861+nml2171@users.noreply.github.com>
2021-07-21 17:06:34 -0400

    ewf

--- ShinyApp/dataLoad.R
+++ ShinyApp/dataLoad.R
@@ -5,10 +5,15 @@ library(stringr)
 
 #load hs data
 hs_data <- read.csv("~/PIT-DSC-STEM-Inequality/data/2018_HS_Data.csv")
+ms_df <- read_excel('~/PIT-DSC-STEM-Inequality/data/2018 MS Metrics (Complete).xlsx')
+csr <- read_excel("~/PIT-DSC-STEM-Inequality/data/Computer Science Report.xlsx", sheet = "STEM Teachers", col_types = c("text", "skip", "text", "skip", "skip", "skip", "text"), skip = 3)
 
 #turn decimal number in the ratio column into fractions, then turn fractions into ratios
 hs_data[,31] <- paste(fractions(hs_data[,31], cycles = 3), " ")
 hs_data[,31] <- str_replace(hs_data[,31],"/", ":")
+
+ms_df <- ms_df %>% left_join(csr, by = 'DBN')
+ms_df <- subset(ms_df, select = -c(9) )
 
 #remove whitespace from end of string and front of string
 hs_data[,"Borough"] <- hs_data[,"Borough"] %>%  str_trim(side = "both")
@@ -17,5 +22,3 @@ hs_data[,"Borough"] <- hs_data[,"Borough"] %>%  str_trim(side = "both")
 hs_data <- hs_data %>% mutate('Salary Per Classroom Teacher' = Classroom.Teachers_2017.18
                     / (Classroom.Teachers.w..0.3.Years.Experience_2017.18 + Classroom.Teachers.w..More.than.3.Years.Experience_2017.18)
 )
-
-
--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -1,53 +1,76 @@
 #server for our app
-library(shiny)
 
 
 shinyServer( function(input, output){
-     #want reactive object because our map/results will change depending on user
-     #input
-     school_data <- reactive({
-         d <- hs_data %>% filter(( input$percent_poverty[1] <= Percent.Poverty & Percent.Poverty <= input$percent_poverty[2]) &
-                                         (Borough %in% input$borough) &
-                                         (input$ccAlgebra[1] <= Mean.Score_Common.Core.Algebra & Mean.Score_Common.Core.Algebra <= input$ccAlgebra[2])
-                                        &
-                                         (input$ccGeometry[1] <= Mean.Score_Common.Core.Geometry & Mean.Score_Common.Core.Geometry <= input$ccGeometry[2] )
-                                        &
-                                         ((input$ccLE[1] <= Mean.Score_Living.Environment & Mean.Score_Living.Environment <= input$ccLE[2]) ))
-         return(d)
-     })
+        #want reactive object because our map/results will change depending on user
+        #input
+        school_data <- reactive({
+                #creates conditional, if current tab is High School, work with High School data
+                if (input$CurrentTab == "High School") {
+                        d <- hs_data %>% filter(( input$percent_poverty[1] <= Percent.Poverty & Percent.Poverty <= input$percent_poverty[2]) &
+                                                        (Borough %in% input$borough) &
+                                                        (input$ccAlgebra[1] <= Mean.Score_Common.Core.Algebra & Mean.Score_Common.Core.Algebra <= input$ccAlgebra[2])
+                                                &
+                                                        (input$ccGeometry[1] <= Mean.Score_Common.Core.Geometry & Mean.Score_Common.Core.Geometry <= input$ccGeometry[2] )
+                                                &
+                                                        ((input$ccLE[1] <= Mean.Score_Living.Environment & Mean.Score_Living.Environment <= input$ccLE[2]) ))
+                        return(d) }
+        })
 
-     #we want to output a map based on the grade_range input
-     output$map <- renderLeaflet({
-          #this is a template, shows that High school map works when selecting High school input
-           if (input$grade_range == "High School"){
+        #The HS tab will output the map based on user's input
+        output$HS <- renderLeaflet({
+                #make the points of the school a certain shade of the color red depending
+                #on Percent Poverty
+                pal <- colorBin(palette = c('#fff7ec','#fee8c8','#fdd49e','#fdbb84','#fc8d59','#ef6548','#d7301f','#b30000','#7f0000'),9, domain = hs_data$Percent.Poverty)
+
+
+                #this creates a leaflet map to be displayed
                 leaflet() %>%
-                     addTiles() %>%
-                     addCircleMarkers(data = school_data() , lng = ~Longitude, lat = ~Latitude, color = "#2a52be",
-                                      popup = ~paste("<h3 style = 'color: #2a52be'>School Information</h3>",
-                                                     "<b>School Name: </b>",
-                                                     school_name,
-                                                     "<br>",
-                                                     "<b>% of Students in Poverty: </b>",
-                                                     paste(Percent.Poverty * 100,"%"),
-                                                     "<br>",
-                                                     "<b> Ratio of STEM teachers to Students: </b>",
-                                                     Ratio.of.Full.Time.Licensed.STEM.Teachers.to.Students,
-                                                     "<br>",
-                                                     "<b> STEM AP Courses Offered: </b>",
-                                                     STEM_AP_Courses_Offered,
-                                                     sep = " "))
+                        addTiles() %>%
+                        addCircleMarkers(data = school_data() , lng = ~Longitude, lat = ~Latitude, color = ~pal(Percent.Poverty),
+                                         popup = ~paste("<h3 style = 'color: #2a52be'>School Information</h3>",
+                                                        "<b> DBN: </b>",
+                                                        dbn,
+                                                        "<br>",
+                                                        "<b>School Name: </b>",
+                                                        school_name,
+                                                        "<br>",
+                                                        "<b> Mean Score for Algebra Regent: </b> ",
+                                                        Mean.Score_Common.Core.Algebra,
+                                                        "<br>",
+                                                        "<b> Mean Score for Geometry Regent: </b> ",
+                                                        Mean.Score_Common.Core.Geometry,
+                                                        "<br>",
+                                                        "<b> Mean Score For Living Environment Regent: </b>",
+                                                        Mean.Score_Living.Environment,
+                                                        "<br>",
+                                                        "<b>% of Students in Poverty: </b>",
+                                                        paste(Percent.Poverty,"%"),
+                                                        "<br>",
+                                                        "<b> Total School Funding Per Pupil: </b>",
+                                                        Total.School.Funding.per.Pupil_2017.18,
+                                                        "<br>",
+                                                        "<b> Ratio of STEM teachers to Students: </b>",
+                                                        Ratio.of.Full.Time.Licensed.STEM.Teachers.to.Students,
+                                                        "<br>",
+                                                        "<b> STEM AP Courses Offered: </b>",
+                                                        STEM_AP_Courses_Offered,
+                                                        "<br>",
+                                                        "<b> STEM Clubs Offered: </b>",
+                                                        club_number,
+                                                        sep = " ")) %>%
+                        #adds a bottom right label that displays the color with the corresponding
+                        #Percentage of students in poverty
+                        addLegend(position = "bottomright",
+                                  pal = pal,
+                                  values = school_data()$Percent.Poverty,
+                                  title = "Percentage of Students in Poverty",
+                                  opacity = 0.7,
+                        )
 
-           }
-     })
+
+        })
 
 
 
 })
-
-
-
-
-
-
-
-
