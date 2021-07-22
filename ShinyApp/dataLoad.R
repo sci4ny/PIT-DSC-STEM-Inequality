29b4b45ba9636a03bbc8e1a291949a514402140e
ATEOTG <bryansantos9797@gmail.com>
2021-07-21 20:20:09 -0400

    changed comments

--- ShinyApp/dataLoad.R
+++ ShinyApp/dataLoad.R
@@ -4,13 +4,14 @@ library(MASS)
 library(stringr)
 library(readxl)
 
-#load hs data
+#load our school datasets
 hs_data <- read.csv("~/PIT-DSC-STEM-Inequality/data/2018_HS_Data.csv")
 ms_df <- read_excel('~/PIT-DSC-STEM-Inequality/data/2018 MS Metrics (Complete).xlsx')
 
 #turn decimal number in the ratio column into fractions, then turn fractions into ratios
 hs_data[,31] <- paste(fractions(hs_data[,31], cycles = 3), " ")
 hs_data[,31] <- str_replace(hs_data[,31],"/", ":")
+
 
 ms_df <- subset(ms_df, select = -c(9) )
 
