a2ae7504c0ac057848dc8bcee248c200a8b7ab71
ATEOTG <bryansantos9797@gmail.com>
2021-07-21 20:09:50 -0400

    added middle school map

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -4,6 +4,7 @@
 shinyServer( function(input, output){
      #want reactive object because our map/results will change depending on user
      #input
+
      school_data <- reactive({
          #creates conditional, if current tab is High School, work with High School data
          if (input$CurrentTab == "High School") {
@@ -15,9 +16,15 @@ shinyServer( function(input, output){
                                         &
                                          ((input$ccLE[1] <= Mean.Score_Living.Environment & Mean.Score_Living.Environment <= input$ccLE[2]) ))
          return(d) }
+         else if (input$CurrentTab == "Middle School") {
+                            d <- ms_df %>% filter(( input$percent_poverty[1] <=  `Poverty Percent` & `Poverty Percent` <= input$percent_poverty[2]) & (Borough %in% input$borough) & (input$`TSFPP`[1] <= TSFPP & TSFPP <= input$`TSFPP`[2]) & (input$`Scaled Mean Score`[1] <= `Scaled Mean Score` & `Scaled Mean Score` <= input$`Scaled Mean Score`[2]))
+                             return (d) }
+
      })
 
-     #The HS tab will output the map based on user's input
+
+     #The tab will output the map based on user's current tab
+
      output$HS <- renderLeaflet({
                 #make the points of the school a certain shade of the color red depending
                 #on Percent Poverty
@@ -47,12 +54,6 @@ shinyServer( function(input, output){
                                                      "<b>% of Students in Poverty: </b>",
                                                      paste(Percent.Poverty,"%"),
                                                      "<br>",
-                                                     "<b> Total School Funding Per Pupil: </b>",
-                                                     Total.School.Funding.per.Pupil_2017.18,
-                                                     "<br>",
-                                                     "<b> Salary Per Classroom Teache: </b>",
-                                                     `Salary Per Classroom Teacher`,
-                                                     "<br>",
                                                      "<b> Ratio of STEM teachers to Students: </b>",
                                                      Ratio.of.Full.Time.Licensed.STEM.Teachers.to.Students,
                                                      "<br>",
@@ -70,6 +71,52 @@ shinyServer( function(input, output){
                           title = "Percentage of Students in Poverty",
                           opacity = 0.7,
                           )
+
+
+     })
+
+
+
+     output$MS <- renderLeaflet({
+             #make the points of the school a certain shade of the color blue depending
+             #on Percent Poverty
+             pal <- colorBin(palette = c('#fff7fb','#ece7f2','#d0d1e6','#a6bddb','#74a9cf','#3690c0','#0570b0','#045a8d','#023858'),9, domain = ms_df$`Poverty Percent`)
+
+
+             #this creates a leaflet map to be displayed
+             leaflet() %>%
+                     addTiles() %>%
+                     addCircleMarkers(data = school_data() , lng = ~Longitude, lat = ~Latitude, color = ~pal(`Poverty Percent`),
+                                      popup = ~paste("<h3 style = 'color: #2a52be'>School Information</h3>",
+                                                     "<b> DBN: </b>",
+                                                     DBN,
+                                                     "<br>",
+                                                     "<b>School Name: </b>",
+                                                     `School Name`,
+                                                     "<br>",
+                                                     "<b>Percent in Poverty: </b>",
+                                                     paste(`Poverty Percent`,"%"),
+                                                     "</br>",
+                                                     "<b> Total School Funding Per Pupil: </b>",
+                                                     TSFPP,
+                                                     # "<br>",
+                                                     # "<b> Ratio of STEM teachers to Students: </b>",
+                                                     # `Ratio of Full Time Licensed STEM Teachers to Students*`,
+                                                     "<br>",
+                                                     "<b> STEM Clubs Offered: </b>",
+                                                     `Stem Activities Count`,
+                                                     "</br>",
+                                                     "<b> Scaled Mean Math Test Scores </b>",
+                                                     `Scaled Mean Score`,
+                                                     "</br>", sep = " ")) %>%
+                     #adds a bottom right label that displays the color with the corresponding
+                     #Percentage of students in poverty
+                     addLegend(position = "bottomright",
+                               pal = pal,
+                               values = school_data()$`Poverty Percent`,
+                               title = "Percentage of Students in Poverty",
+                               opacity = 0.7
+                     )
 
 
      })
